1deefbb399d9234a1a193a8e741540ea8dd3bf9d
Park, Jimin <qoarhkdid@gmail.com>
2026-03-25 09:40:20 +0900

    main 메소드 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,9 +1,11 @@
 package git_test2;
 
 public class Main {
-
     public static void main(String[] args) {
-        new Hello().say();
+        // Hello 클래스의 인스턴스를 생성합니다.
+        Hello h = new Hello();
+        
+        // say() 메소드를 호출하여 메시지를 출력합니다.
+        h.say();
     }
-
 }
